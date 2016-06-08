09171720450b11c8f10804babc51ef09d3ae40d9
dmitrydi <d.dmitrook@gmail.com>
2016-06-08 12:57:49 +0400

    Specs for MovieToShow and AncientMovie changed

--- task_6/spec/ancient_movie_spec.rb
+++ task_6/spec/ancient_movie_spec.rb
@@ -1,12 +1,33 @@
-require_relative 'movies_shared_spec'
+require_relative '..\movie_classes'
+require 'csv'
 
 describe AncientMovie do
 
-  it_behaves_like "a movie with limited period and certain price", 1900, 1945, 'ancient', 1
+  let(:record) { CSV.read('../movies.txt', col_sep: '|').map.find{|a| AncientMovie::PERIOD.include?(a[2].to_i)} }
+  let(:bad_record) { CSV.read('../movies.txt', col_sep: '|').map.find{|a| !(AncientMovie::PERIOD.include?(a[2].to_i))} }
+  let(:ancient_movie) {AncientMovie.new(record)}
+
+  it { expect(record).not_to be_nil }
+  it { expect(AncientMovie::PERIOD).to eq(1900..1945)}
+  it { expect{AncientMovie.new(bad_record)}.to raise_error(ArgumentError) }
+  it { expect{ancient_movie}.not_to raise_error }
+
+  describe '#period' do
+  	it "returns 'ancient' value" do
+  	  expect(ancient_movie.period).to eq('ancient')
+  	end
+  end
+
+  describe '#price' do
+  	it "returns 1" do
+  	  expect(ancient_movie.price).to eq(1)
+  	end
+  end
 
   describe '#to_s' do
-    include_examples "prepare an instance", 1900, 1945, :movie
-    it { expect(movie.to_s).to eq("#{movie.title} - old movie (#{movie.year})") }
+  	it "returns a string '#{@title} - old movie (#{@year})' " do
+  	  expect(ancient_movie.to_s).to eq("#{ancient_movie.title} - old movie (#{ancient_movie.year})")
+    end
   end
 
 end
--- task_6/spec/movie_to_show_spec.rb
+++ task_6/spec/movie_to_show_spec.rb
@@ -6,58 +6,20 @@ require 'date'
 
 describe MovieToShow do
 
-  describe "right_year?" do
-    let(:record) {CSV.read('..\movies.txt', col_sep: "|")[0]}
-    let(:movie_to_show) {MovieToShow.new(record)}
-    let(:year) {movie_to_show.year.to_i}
+  let(:record) {CSV.read('../movies.txt', col_sep: '|')[0]}
+  let(:movie) { MovieToShow.new(record) }
+  let(:ancient_record) { CSV.read('../movies.txt', col_sep: '|').map.find{|a| AncientMovie::PERIOD.include?(a[2].to_i)} }
 
-    it "returns nil when @year in specified range" do
-      expect(movie_to_show.right_year?(year-1, year+1)).to be_nil
-    end
+  describe '#check_year' do
+    it { expect{movie.check_year(1900)}.not_to raise_error }
+  end
 
-    it "raise an ArgumentError when @year is out of specified range" do
-      expect{ movie_to_show.right_year?(year-2, year-1) }.to raise_error(ArgumentError, "year should be in range #{year-2}..#{year-1}")
+  describe '#create' do
+    it 'creates an AncientMovie instance when period is ancient' do
+      expect(MovieToShow.create(ancient_record)).to be_instance_of AncientMovie
     end
   end
 
-  describe "#create" do
 
-    context "for 1900..1945 creates AncientMovie" do
-      include_examples "creates a movie of appropriate class", 1900, 1945, AncientMovie
-    end
-
-    context "for 1946..1968 creates ClassicMovie" do
-      include_examples "creates a movie of appropriate class", 1946, 1968, ClassicMovie
-    end
-
-    context "for 1969..2000 creates ModernMovie" do
-      include_examples "creates a movie of appropriate class", 1969, 2000, ModernMovie
-    end
-
-    context "for 2001..2016 creates ModernMovie" do
-      include_examples "creates a movie of appropriate class", 2001, 2016, NewMovie
-    end
-
-    context "for bad year value raises an ArgumentError" do
-      let(:bad_record) {CSV.read('..\movies.txt', col_sep: "|")[0]} 
-
-      it "expect ot raise error for year value > 2016" do
-        bad_record[2] = "2017" 
-        expect{MovieToShow.create(bad_record)}.to raise_error(ArgumentError, "Error in MovieToShow#create: unrecognized value of year")
-      end
-
-      it "expect ot raise error for year value < 1900" do
-        bad_record[2] = "1800" 
-        expect{MovieToShow.create(bad_record)}.to raise_error(ArgumentError, "Error in MovieToShow#create: unrecognized value of year")
-      end
-
-      it "expect ot raise error for if year value is missing" do
-        bad_record[2] = "" 
-        expect{MovieToShow.create(bad_record)}.to raise_error(ArgumentError, "Error in MovieToShow#create: unrecognized value of year") 
-      end
-    end
-
-
-  end
 
 end
