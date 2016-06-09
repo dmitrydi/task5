e1edbc147e5ff84978f8b598f001456a6c96eb5c
dmitrydi <d.dmitrook@gmail.com>
2016-06-09 19:11:38 +0400

    specs netfix and moviecollection update

--- task_6/spec/netfix_spec.rb
+++ task_6/spec/netfix_spec.rb
@@ -11,8 +11,8 @@ describe Netfix do
     include_examples "creates a collection of diff.movies", Netfix.new.read
   end
 
-  describe 'self.read' do
-    include_examples "creates a collection of diff.movies", Netfix.read
+  describe '#self.read' do
+    it {expect(netfix).to be_instance_of Netfix}
   end
 
   describe '#pay' do
@@ -21,31 +21,46 @@ describe Netfix do
     it { expect(netfix.pay(10)).to be_instance_of Netfix }
   end
 
-  describe '#films_by_producers' do
-    subject {netfix.films_by_producers}
-    it { is_expected.to be_instance_of Hash}
-    it { is_expected.to all(be_instance_of Array)}
-  end
-
   describe '#make_shortlist' do
 
     it { expect(netfix.make_shortlist).to be_instance_of Array }
 
     context 'when money = 0 returns an empty array' do
-      it { expect(netfix.make_shortlist).to be_empty }
+     it { expect(netfix.make_shortlist.length).to eq(0) }
+    end
+
+    context 'it raise error with bad filter' do
+      it { expect{netfix.make_shortlist(genre: 'Tragedy')}.to raise_error(ArgumentError)}
     end
 
     context 'when sufficient money array of movies is not empty' do
-      #it { expect(@netfix.pay(10).make_shortlist).not_to be_empty}
+      it { expect(netfix.pay(10).make_shortlist).not_to be_empty}
     end
 
     context 'when money = 1 return only array of AncientMovie' do
-      it { expect(netfix.pay(1).make_shortlist[0]).to be_instance_of AncientMovie }
+      it { expect(netfix.pay(1).make_shortlist).to all(be_instance_of(AncientMovie)) }
     end
+
+    context 'filters by period' do
+      it { expect(netfix.pay(10).make_shortlist(period: 'ancient')).to all(be_instance_of(AncientMovie)) }
+    end
+
+    context 'works with filters' do
+      it { expect(netfix.pay(10).make_shortlist(country: 'USA')).to all(have_attributes(:country => 'USA')) }
+    end
+
+    context 'when money = 1.5 return array of Ancient and Classic movies' do
+      it { expect(netfix.pay(1.5).make_shortlist).to all(be_instance_of(AncientMovie) | be_instance_of(ClassicMovie)) }
+    end
+
   end
 
-
-
-
+  describe '#how_much?' do
+    it 'expect to return right price' do
+      price = netfix.filter(title: "The Terminator").collection.first.price
+      expect(netfix.how_much?("The Terminator")).to eq(price) 
+    end
+    it { expect{netfix.how_much?("Santa Barbara")}.to raise_error(ArgumentError)}
+  end
 
 end
