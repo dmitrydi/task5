1a012ae263b773e006d60509f5f98bffd72d8ffb
dmitrydi <d.dmitrook@gmail.com>
2016-06-17 17:06:00 +0400

    Theatre class changed

--- task_6/theatre.rb
+++ task_6/theatre.rb
@@ -6,38 +6,28 @@ MORNING = 8..11
 NOON = 12..17
 EVENING = 18..23
 NIGHT = 0..2
+FILTERS_FOR_PERIODS = { MORNING => {period: 'ancient'}, NOON => {genre: ['Comedy', 'Adventure']}, [EVENING, NIGHT] => {genre: ['Drama', 'Horror']} }
   def select_movie(time)
     raise ArgumentError, "enter time from 08:00 to 02:59" unless time
-    list_to_show = case Time.parse(time).hour
-                    when MORNING
-                      self.filter(period: 'ancient').collection
-                    when NOON
-                      self.filter(genre: 'Comedy').collection + self.filter(genre: 'Adventure').collection
-                    when EVENING, NIGHT
-                      self.filter(genre: 'Drama').collection + self.filter(genre: 'Horror').collection
-                    else
-                      raise ArgumentError, "The cinema is closed from 03:00 to 07:59"
-                   end
-    Theatre.new(list_to_show).pay(@money).filter_by_price(@money).collection.max_by{ |a| rand * a.rating }
+    hour = Time.parse(time).hour
+    filter = FILTERS_FOR_PERIODS.find{ |k, v| k.is_a?(Array) ? k.inject(false) { |m,a| m || a.include?(hour) } : k.include?(hour) }
+    raise ArgumentError, "The cinema is closed from 03:00 to 07:59" unless filter
+    list_to_show = self.filter(filter[1]).collection
+    Theatre.new(list_to_show).collection.max_by{ |a| rand * a.rating }
   end
 
 
   def time_for(title)
     movie = self.filter(title: title).collection.first
-    time = if movie.period == 'ancient'
-             rand(MORNING)  
-           else
-             genre = movie.genre
-           case 
-             when genre.include?('Comedy'), genre.include?('Adventure')
-               rand(NOON)
-             when genre.include?('Drama'), genre.include?('Horror')
-               rand(0..1) == 0 ? rand(EVENING) : rand(NIGHT)
-             else
-               raise ArgumentError, "film is not in the schedule"
-           end
-          end
-    time <= 9 ? ("0%d:00" %time) : ("%d:00" %time)
+    period = FILTERS_FOR_PERIODS.find { |k,v| movie.any_match?(v.keys.first, v.values.first) }
+    raise ArgumentError, "Film is not in the schedule" unless period
+    period = period[0].is_a?(Array) ? period[0][rand(0..1)] : period[0]
+    time = rand(period)
+    ("%02d:00" %time) 
+  end
+
+  def show(time)
+    puts "Now showing: " + select_movie(time).to_s
   end
 
 end
