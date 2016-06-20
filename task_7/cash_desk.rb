45103be4f56d05d3ccee9a52c205a2bb07ef8ae5
dmitrydi <d.dmitrook@gmail.com>
2016-06-20 17:49:19 +0400

    CashDesk update
    to be used both for instances and classes

--- task_7/cash_desk.rb
+++ task_7/cash_desk.rb
@@ -1,7 +1,31 @@
 module CashDesk
 
-  def cash
-    self.cinema_cash
+  def self.included(base)
+    base.extend(ClassMethods)
   end
+
+  attr_reader :cash
+
+  def cash
+    @cash ||= 0
+  end
+
+  def put_cash(amount)
+    raise ArgumentError, "Amount of cash should be >= 0" if amount < 0
+    @cash = cash + amount
+  end
+
+    module ClassMethods
+      @@cash = 0 
+
+      def cash
+        @@cash
+      end
+
+      def put_cash(amount)
+        raise ArgumentError, "Amount of cash should be >= 0" if amount < 0
+        @@cash += amount
+      end
+    end
 
 end
