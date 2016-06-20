6d573077b1b109fe67b6185955b838d709cbecb0
dmitrydi <d.dmitrook@gmail.com>
2016-06-20 18:52:53 +0400

    CashDesk simplified

--- task_7/cash_desk.rb
+++ task_7/cash_desk.rb
@@ -1,11 +1,5 @@
 module CashDesk
 
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  attr_reader :cash
-
   def cash
     @cash ||= 0
   end
@@ -14,18 +8,5 @@ module CashDesk
     raise ArgumentError, "Amount of cash should be >= 0" if amount < 0
     @cash = cash + amount
   end
-
-    module ClassMethods
-      @@cash = 0 
-
-      def cash
-        @@cash
-      end
-
-      def put_cash(amount)
-        raise ArgumentError, "Amount of cash should be >= 0" if amount < 0
-        @@cash += amount
-      end
-    end
 
 end
