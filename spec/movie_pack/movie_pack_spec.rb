0d73dc62fe1db76b108291824dd2060db6e1ce8d
dmitrydi <d.dmitrook@gmail.com>
2016-06-21 18:50:28 +0400

    movie_pack_spec update

--- spec/movie_pack/movie_pack_spec.rb
+++ spec/movie_pack/movie_pack_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/movie_pack'
+require_relative '../../lib/movie_pack'
 
 describe MoviePack do 
   
@@ -7,7 +7,7 @@ describe MoviePack do
     let(:movie) {MoviePack::Movie.new(record)}
 
     it{ expect(movie).to be_instance_of MoviePack::Movie }
-    it{ expect(movie).to have_attribute(:webaddr => record[0]) }
+    #it{ expect(movie).to have_attribute(:webaddr => record[0]) }
   end
   
 end
