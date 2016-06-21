6e32aec8ff1041ea3a12b5e34ec4c6c99506f0c6
dmitrydi <d.dmitrook@gmail.com>
2016-06-21 19:21:10 +0400

    spec update

--- spec/movie_pack/movie_pack_spec.rb
+++ spec/movie_pack/movie_pack_spec.rb
@@ -7,7 +7,22 @@ describe MoviePack do
     let(:movie) {MoviePack::Movie.new(record)}
 
     it{ expect(movie).to be_instance_of MoviePack::Movie }
-    #it{ expect(movie).to have_attribute(:webaddr => record[0]) }
+    it{ expect(movie).to respond_to(:webaddr, :title, :year, :country, :date, :genre, :duration, :rating, :producer, :actors, :month) }
+
+    describe '#has_genre?' do
+      context 'when initialized without host' do
+        it{ expect(movie.has_genre?(movie.genre)).to be_truthy }
+        it{ expect(movie.has_genre?(movie.genre[0])).to be_truthy }
+        it{ expect(movie.has_genre?('Tragedy')).to be false }
+      end
+
+      context 'when initialized with host' do
+        let(:movie_collection) { MoviePack::MovieCollection.read }
+        let(:movie_with_host) { MoviePack::Movie.new( record, movie_collection ) }
+        it{ expect{ movie_with_host.has_genre?('Tragedy') }.to raise_error ArgumentError }
+      end
+    end
+
   end
   
 end
