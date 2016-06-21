62e7d59e386f43d19cc03e0e4363ed738dc6de76
dmitrydi <d.dmitrook@gmail.com>
2016-06-21 10:33:03 +0400

    spec folder created

---
+++ spec/ancient_movie_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'movies_shared_spec'
+
+describe AncientMovie do
+
+  period = AncientMovie::PERIOD
+
+  let(:ancient_movie) {make_movie(described_class)}
+
+  it_behaves_like "a movie with limited period and certain price", period, 'ancient', 1
+
+  describe '#to_s' do
+  	it { expect(ancient_movie.to_s).to eq("#{ancient_movie.title} - old movie (#{ancient_movie.year})") }
+  end
+
+end
