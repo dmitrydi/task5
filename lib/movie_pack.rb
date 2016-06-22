24fb7bb598aaaafee28e64192496a7f267e46962
dmitrydi <d.dmitrook@gmail.com>
2016-06-22 14:58:15 +0400

    movie_pack update

--- lib/movie_pack.rb
+++ lib/movie_pack.rb
@@ -1,5 +1,16 @@
 require_relative 'movie_pack/movie'
 require_relative 'movie_pack/movie_collection'
+require_relative 'movie_pack/ancient_movie'
+require_relative 'movie_pack/cash_desk'
+require_relative 'movie_pack/cinema'
+require_relative 'movie_pack/classic_movie'
+require_relative 'movie_pack/error_classes'
+require_relative 'movie_pack/modern_movie'
+require_relative 'movie_pack/movie_classes'
+require_relative 'movie_pack/movie_to_show'
+require_relative 'movie_pack/netfix'
+require_relative 'movie_pack/new_movie'
+require_relative 'movie_pack/theatre'
 
 module MoviePack
  MOVIEFILE = '../../src/movie_pack/movies.txt'
