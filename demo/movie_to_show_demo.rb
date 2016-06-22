51ef837ecd6e3a4553e204f4ed2c2c9e18983b73
dmitrydi <d.dmitrook@gmail.com>
2016-06-22 15:25:52 +0400

    Delete movie_to_show_demo.rb

--- demo/movie_to_show_demo.rb
+++
@@ -1,9 +0,0 @@
- require_relative 'movie_to_show'
-
- record = CSV.read('..\src\movies.txt', col_sep: '|')[0]
-    
- movieshow = MovieToShow.new(record)
-
- puts movieshow.year
- puts movieshow.period
- puts movieshow.to_s
