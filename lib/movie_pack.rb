ec65a0a4d9104d6ab534b819ee74efb4e1b70c28
dmitrydi <d.dmitrook@gmail.com>
2016-06-22 17:25:38 +0400

    movie_pack update to use File#expend_path

--- lib/movie_pack.rb
+++ lib/movie_pack.rb
@@ -4,7 +4,6 @@ require_relative 'movie_pack/ancient_movie'
 require_relative 'movie_pack/cash_desk'
 require_relative 'movie_pack/cinema'
 require_relative 'movie_pack/classic_movie'
-require_relative 'movie_pack/error_classes'
 require_relative 'movie_pack/modern_movie'
 require_relative 'movie_pack/movie_classes'
 require_relative 'movie_pack/movie_to_show'
@@ -13,5 +12,8 @@ require_relative 'movie_pack/new_movie'
 require_relative 'movie_pack/theatre'
 
 module MoviePack
- MOVIEFILE = '../../src/movie_pack/movies.txt'
+  MOVIEFILE = File.expand_path('../../data/movie_pack/movies.txt', __FILE__)
+
+  class EncashmentError < StandardError
+  end
 end
