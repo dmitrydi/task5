d9ca2de651ad31e83c3a599194ac52e98c4cdf32
dmitrydi <d.dmitrook@gmail.com>
2016-07-01 11:10:13 +0400

    shared specs fixed to init Movie with hash

--- spec/shared.rb
+++ spec/shared.rb
@@ -7,15 +7,24 @@ RSpec.shared_examples "creates a collection of diff.movies" do |cinema|
 end
 
 RSpec.shared_examples "a movie with limited period and certain price" do |year_range, expected_period, expected_price|
-  let(:movies_file) {MoviePack::MOVIEFILE}
-  let(:record) {CSV.read(movies_file, col_sep: "|").map.find{|a| year_range.include?(a[2].to_i)}}
-  let(:bad_record) {CSV.read(movies_file, col_sep: "|").map.find{|a| !(year_range.include?(a[2].to_i))}}
-  let(:movie) {described_class.new(record)}
+  let(:movies_file) { MoviePack::MOVIEFILE }
+  let(:rec_headers) { MoviePack::REC_HEADERS }
+  let(:record) do
+    CSV.read(movies_file, col_sep: "|", headers: rec_headers)
+       .map
+       .find{ |a| year_range.include?(a[:year].to_i) }
+  end
+  let(:bad_record) do
+    CSV.read(movies_file, col_sep: "|", headers: rec_headers)
+    .map
+    .find{ |a| !(year_range.include?(a[2].to_i)) }
+  end
+  let(:movie) { described_class.new(record) }
 
   it{ expect(movie).to be_an_instance_of described_class }
 
   describe "#initialize and #check_year" do 
-    it {expect{described_class.new(bad_record)}.to raise_error(ArgumentError)}
+    it { expect{described_class.new(bad_record)}.to raise_error(ArgumentError) }
   end 
 
   describe '#period' do
@@ -33,12 +42,22 @@ RSpec.shared_examples "a movie with limited period and certain price" do |year_r
 end
 
 RSpec.shared_examples "creates a movie of appropriate class" do |year_range, class_name|
-  let(:record) {CSV.read(MoviePack::MOVIEFILE, col_sep: "|").map.find{|a| year_range.include?(a[2].to_i)}}
+  let(:record) do
+    CSV.read(MoviePack::MOVIEFILE, col_sep: "|", headers: MoviePack::REC_HEADERS)
+       .map
+       .find{ |a| year_range.include?(a[:year].to_i) }
+  end
   it { expect(described_class.create(record)).to be_an_instance_of class_name }
 end
 
 def make_movie(movie_class, host = nil)
-  record = CSV.read(MoviePack::MOVIEFILE, col_sep: "|").map.find{ |a| movie_class::PERIOD.include?(a[2].to_i) }
+  record = CSV.read(
+                    MoviePack::MOVIEFILE, col_sep: "|",
+                    headers: MoviePack::REC_HEADERS
+                   )
+              .map
+              .find{ |a| movie_class::PERIOD.include?(a[:year].to_i) }
+
   movie_class.new(record, host)
 end
 
