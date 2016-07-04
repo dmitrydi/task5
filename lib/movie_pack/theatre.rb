123bdb51c3f2f4402bb12d2c56cc2abf925ae8d4
dmitrydi <d.dmitrook@gmail.com>
2016-07-04 16:41:32 +0400

    #hall, #period, Container added

--- lib/movie_pack/theatre.rb
+++ lib/movie_pack/theatre.rb
@@ -18,6 +18,68 @@ module MoviePack
       NIGHT => { genre: %w(Drama Horror) }
     }.freeze
 
+    def initialize(movie_array = nil, &block)
+      super(movie_array)
+      @halls = {}
+      @periods = {}
+      if block_given?
+        instance_eval(&block)
+      end
+    end
+
+    attr_reader :halls, :periods
+
+    def hall(name, places: 0, title: '')
+      @halls.merge!(name => [title, places])
+      self
+    end
+
+    def period(name, &block)
+      container = Container.new
+      container.fill(&block)
+      @periods.merge!(name => container)
+    end
+
+    class Container
+      def initialize
+        @description = ''
+        @filters = {}
+        @price = 0
+        @hall = []
+      end
+
+      attr_reader :description, :filters, :price, :hall
+
+      def fill(&block)
+        instance_eval(&block)
+      end
+
+      def description(a_description = nil)
+        return @description unless a_description
+        @description = a_description
+        self
+      end
+
+      def filters(*some_filters)
+        return @filters if some_filters.empty?
+        some_filters.each { |filt| @filters.merge!(filt) }
+        self
+      end
+
+      def price(amount = nil)
+        return @price unless amount
+        @price = amount
+        self
+      end
+
+      def hall(*list)
+        return @hall if list.empty?
+        list.each { |val| @hall << val }
+        self
+      end
+    end
+
+
     def select_movie(time = nil)
       raise ArgumentError, 'Enter time from 08:00 to 02:59' unless time
       hour = Time.parse(time).hour
