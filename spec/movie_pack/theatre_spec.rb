f9b653dd665478628d2bddbd2fdb2f8e38fbad21
dmitrydi <d.dmitrook@gmail.com>
2016-07-06 10:09:52 +0400

    new checks added for TheatreBuilder work

--- spec/movie_pack/theatre_spec.rb
+++ spec/movie_pack/theatre_spec.rb
@@ -6,7 +6,8 @@ describe Theatre do
 
   describe '#initialize with block' do
     it { expect(Theatre.new { hall(:red, places: 100, title: 'Red Hall') }.halls.length).to eq(1) }
-    it { expect(Theatre.new { hall(:red, places: 100, title: 'Red Hall')}.halls[:red]).to eq(['Red Hall', 100]) }
+    it { expect(Theatre.new { hall(:red, places: 100, title: 'Red Hall') }.halls[:red]).to eq(['Red Hall', 100]) }
+
     context '#period check' do
       let(:theatre) do
         Theatre.new do
@@ -23,11 +24,25 @@ describe Theatre do
 
       let(:time_range) { '09:00'..'11:00' }
       subject { theatre.periods[time_range] }
-      
+
       it { expect(subject.description).to eq('Morning') }
       it { expect(subject.filters).to eq( { genre: 'Comedy', year: 1900..1950 } ) }
       it { expect(subject.price).to eq(10) }
       it { expect(subject.hall).to eq([:red, :blue]) }
+
+      let(:new_theatre) { Theatre.new }
+      it { expect { new_theatre.hall :red, places: 10, title: 'Red Hall' }.to raise_error(NoMethodError) }
+      it do
+        expect do
+          new_theatre.period '09:00'..'11:00', proc {
+                                                      description('Morning')
+                                                      filters(genre: 'Comedy')
+                                                      price(10) 
+                                                      hall(:red)
+                                                    }
+        end 
+        .to raise_error(NoMethodError)
+      end
     end
   end
 
