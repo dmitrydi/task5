8655c619aad66ea571e052b41eac515a3d21e963
dmitrydi <d.dmitrook@gmail.com>
2016-07-14 18:14:46 +0400

    IMDBBudgets#to_file added

--- lib/imdb_budgets.rb
+++ lib/imdb_budgets.rb
@@ -3,32 +3,42 @@ require 'nokogiri'
 require 'open-uri'
 require_relative 'web_helper'
 require 'progress-bar'
+require 'yaml'
 
 module IMDBBudgets
 
   TOP_250_URL = 'http://www.imdb.com/chart/top?ref_=nv_mv_250_6'
+  BASE_PATH = File.expand_path('../../data/IMDBBudgets/', __FILE__)
+  DEFAULT_NAME = 'budgets.yml'
 
   class Nokogiri::HTML::Document
-    def get_budget
+    def fetch_budget
       self
       .css('div.txt-block:contains("Budget:")')
       .text
       .delete("\n")
-      .gsub(/\s+/, ' ')
+      .gsub(/\s+/, '')
       .gsub('(estimated)', '')
+      .gsub('Budget:', '')
     end
   end
 
   def self.fetch(top_chart_url)
-    pb = ProgressBar.new 250, "Start getting budgets"
-
     main_page = Nokogiri::HTML(WebHelper.cashed_get(top_chart_url))
+    data_ary = []
+
     main_page.css('td.titleColumn a').map do |blok|
       film_url = WebHelper::SITE_STR + blok['href']
       film_page = Nokogiri::HTML(WebHelper.cashed_get(film_url))
-      budget = film_page.get_budget
-      [blok.text, budget]
-      pb.i += 1
+      budget = film_page.fetch_budget
+      data_ary << {name: blok.text, budget: budget}
     end
+  end
+
+  def self.to_file(top_chart_url, name = nil)
+    file_name = File.join(BASE_PATH, name || DEFAULT_NAME)
+    contents = self.fetch(top_chart_url).to_yaml
+    FileUtils.mkdir_p BASE_PATH
+    File.write(file_name, contents)
   end
 end
--- spec/imdb_budgets/imdb_budgets_spec.rb
+++ spec/imdb_budgets/imdb_budgets_spec.rb
@@ -2,12 +2,24 @@ require_relative '../../lib/imdb_budgets'
 require 'rspec/its'
 
 describe IMDBBudgets do
-  describe '#budgets' do
-    let(:url) { IMDBBudgets::TOP_250_URL }
+  let(:url) { IMDBBudgets::TOP_250_URL }
+
+  describe '#fetch' do
     let(:budget_ary) { IMDBBudgets.fetch(url) }
 
     subject { budget_ary }
       its("class") { should eq(Array) }
       its("length") { should eq(250) }
   end
+
+  describe '#to_file' do
+    let(:file_name) { File.join(IMDBBudgets::BASE_PATH, IMDBBudgets::DEFAULT_NAME) }
+    
+    before(:example) do
+      IMDBBudgets.to_file(url)
+    end
+
+    it { expect(File.exists?(file_name)).to be true }
+  end
+
 end
