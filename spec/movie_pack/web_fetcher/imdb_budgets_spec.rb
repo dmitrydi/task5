1758a66e92418907fc4c25499c0ecdb1f99dc0e8
dmitrydi <d.dmitrook@gmail.com>
2016-09-01 16:46:25 +0400

    IMDBBudgets spec fixed

--- spec/movie_pack/web_fetcher/imdb_budgets_spec.rb
+++ spec/movie_pack/web_fetcher/imdb_budgets_spec.rb
@@ -1,34 +1,36 @@
 require_relative 'spec_helper'
 
-describe IMDBBudgets do
-  let(:url) { TOP_250_URL }
+module MoviePack::WebFetcher
+  describe IMDBBudgets do
+    let(:url) { TOP_250_URL }
 
-  describe '#fetch', :vcr do
-    let(:fetch_budgets) { IMDBBudgets.fetch(url) }
+    describe '#fetch', :vcr do
+      let(:fetch_budgets) { IMDBBudgets.fetch(url) }
 
-    context 'when no files exist' do
-      before(:example) do
-        Dir.foreach(TMP_PATH) { |f| File.delete(File.join(TMP_PATH, f)) if f.include?('.html') }
+      context 'when no files exist' do
+        before(:example) do
+          Dir.foreach(TMP_PATH) { |f| File.delete(File.join(TMP_PATH, f)) if f.include?('.html') }
+        end
+
+        it { expect(fetch_budgets).to have_requested(:get, /http:\/\/www.imdb.com.*/).times(251) }
       end
 
-      it { expect(fetch_budgets).to have_requested(:get, /http:\/\/www.imdb.com.*/).times(251) }
+      context 'when temporary files already exist' do
+        subject { fetch_budgets }
+        it { is_expected.to not_have_requested(:any, /http:\/\/www.imdb.com.*/) }
+        its("class") { should eq(Array) }
+        its("length") { should eq(250) }
+      end
     end
 
-    context 'when temporary files already exist' do
-      subject { fetch_budgets }
-      it { is_expected.to not_have_requested(:any, /http:\/\/www.imdb.com.*/) }
-      its("class") { should eq(Array) }
-      its("length") { should eq(250) }
+    describe '#to_file' do
+      let(:file_name) { DEFAULT_BUDGETS_FILE }
+
+      before(:example) do
+        IMDBBudgets.to_file(top_chart_url: url)
+      end
+
+      it { expect(File.exists?(file_name)).to be true }
     end
-  end
-
-  describe '#to_file' do
-    let(:file_name) { File.join(DATA_PATH, IMDBBudgets::DEFAULT_NAME) }
-
-    before(:example) do
-      IMDBBudgets.to_file(url)
-    end
-
-    it { expect(File.exists?(file_name)).to be true }
   end
 end
